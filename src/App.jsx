f552734e1bb0d6e7490cab581a5166ea22fd8307
Vivek Singh <vivekkumarhcc245@gmail.com>
2026-05-11 13:45:01 +0530

    Understanding jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Login,{Profile,Setting, userKey} from './UserLogin'
+import React from 'react'
+
 const App = () => {
+
+  const userName = "Vivek Singh";
+  let x = 20;
+  let y = 30;
+
+
   return (
     <div>
-      <Login />
-      <Profile />
-      <Setting />
-      <h1>{userKey}</h1>
+      <h1>{userName}</h1>
+      <h1>{x + y}</h1>
+      <button onClick={()=>alert("Hii Vivek")}>Click</button>
     </div>
   )
 }
