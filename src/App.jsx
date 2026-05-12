f67702dbba033c8da041434ace306871a2a9efb5
Vivek Singh <vivekkumarhcc245@gmail.com>
2026-05-12 12:34:33 +0530

    nested conditon in useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import Toggle from "./Toggle"
-import { useState } from "react"
+import React, { useState } from 'react'
 
 const App = () => {
-  const [display, setDisplay] = useState(true)
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>Toggle in react js</h1>
-      <button onClick={() => setDisplay(!display)}>toggle</button>
-      {display? <Toggle /> : null}
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count + 1)}>Counter</button>
+      {
+        count == 0 ? <h1>Condition 0</h1>
+        : count == 1 ? <h1>Condition 1</h1>
+        : count == 2 ? <h1>Condition 2</h1>
+        : count == 3 ? <h1>Condition 3</h1>
+        : <h1>Others Condition</h1>
+      }
     </div>
   )
 }
 
-export default App;
+export default App
